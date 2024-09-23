bc671ccc630d8fe8b35b8212214f68630c3cc472
jackieliang5985 <dinowoolly79@gmail.com>
2024-09-23 14:43:05 -0400

    Finished the grammer checks

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -38,17 +38,16 @@ public class Main {
      */
     public static void runProgram(Translator translator) {
         while (true) {
+            String quit = "quit";
             String country = promptForCountry(translator);
-            // TODO CheckStyle: The String "quit" appears 3 times in the file.
-            // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-            if (country.equals("quit")) {
+            if (quit.equalsIgnoreCase(country)) {
                 break;
             }
             // TODO Task: Once you switch promptForCountry so that it returns the country
             //            name rather than the 3-letter country code, you will need to
             //            convert it back to its 3-letter country code when calling promptForLanguage
             String language = promptForLanguage(translator, country);
-            if (language.equals("quit")) {
+            if (quit.equalsIgnoreCase(language)) {
                 break;
             }
             // TODO Task: Once you switch promptForLanguage so that it returns the language
@@ -61,7 +60,7 @@ public class Main {
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
 
-            if ("quit".equals(textTyped)) {
+            if (quit.equals(textTyped)) {
                 break;
             }
         }
